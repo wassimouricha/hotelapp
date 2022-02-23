763a4ea1a0aca456c83256124b849c97646f7ab9
wassimouricha <wbouricha5@gmail.com>
2022-02-23 11:43:54 +0100

    adding colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+const d_blue = Color(0xFF136377);
+
 void main() {
   runApp(MonApp());
 }
@@ -10,9 +12,12 @@ class MonApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(  //literallement une application qui utilise le material design
       debugShowCheckedModeBanner: false, //permet de retirer la banniere debug en haut a droite
-      title: "NC Hotel booking ",
-      home: Container(
-        color: Color(0xFFfcec0c),
+      title: "NC Hotel Booking ",
+      home: Scaffold(
+        backgroundColor: d_blue,
+        
+        appBar: AppBar(title: Text("NC Hotel Booking"), backgroundColor: Color(0xFFfcec0c),),
+
       ),
     ); 
    
