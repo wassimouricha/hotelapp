030c7f1c2a06bf5a96ff99baa261d1b6bfb69994
wassimouricha <wbouricha5@gmail.com>
2022-02-25 11:17:27 +0100

    slider

--- lib/konpeki.dart
+++ lib/konpeki.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hotelapp/main.dart';
@@ -8,19 +11,73 @@ const d_redus = Color(0xFFFF4A57);
 const d_yellow = Color(0xFFfcec0c);
 
 
+class DataModel {
+  final String title;
+  final String imageName;
+  DataModel(
+    this.title,
+    this.imageName
+  );
+}
 
-class Konpeki extends StatelessWidget {
+List<DataModel> dataList = [
+
+DataModel("Konpeki plaza","assets/hotel1.jpg"),
+DataModel("Hoteru","assets/hotel2.jpeg"),
+DataModel("Biotechnica Hotel","assets/hotel3.jpg"),
+DataModel("No Tell Motel","assets/hotel4.jpg"),
+
+
+];
+
+class HomeScreen extends StatefulWidget {
+const HomeScreen({Key? key}) : super(key: key);
+
+@override
+
+_Konpeki createState() => _Konpeki();
+
+}
+
+
+
+class _Konpeki extends State<HomeScreen> {
+
+  late PageController _pageController;
+  int _currentPage = 0;
+  
+  @override
+  void initState(){
+
+    super.initState();
+    _pageController = PageController(initialPage: _currentPage, viewportFraction: 0.8);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor:  Colors.grey[100],
         appBar: MyAppBar(),
         body: SingleChildScrollView(
+          
           child: //SingleChildScrollView permet d'empecher l'erreur d'overflow et donc de scroller
+          
               Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                
             children: [
+
               //car on va avoir plusieurs widget à l'intérieur
-             slider(),
+              titre(),
+              PageView.builder(
+                itemCount: dataList.length,
+                physics: const ClampingScrollPhysics(),
+                controller: ,
+                itemBuilder: (context, index){
+                  return slider(index);
+                },
+                ),
+             
             ],
           ),
         ));
@@ -31,7 +88,7 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   Size get preferredSize => new Size.fromHeight(50);
 
   String titreDeApp = "NC Hotel Booking";
-  String explore = "Konpeki PLaza";
+  String explore = "Description";
 
   @override
   Widget build(BuildContext context) {
@@ -77,13 +134,32 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   }
 }
 
-class slider extends StatelessWidget {
+class titre extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200,
-      color: Colors.grey[100],
+     
+       
+                padding: EdgeInsets.symmetric(vertical: 40),
+                child:
+                Text("Retrouvez vos hotels préférés de Night City",
+              style: GoogleFonts.nunito(
+                          
+                          color: d_lightblue,
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                        ),),
+              
      
     );
   }
 }
+
+
+  Widget slider(int index) {
+    return Container(
+
+      
+     
+    );
+  }
