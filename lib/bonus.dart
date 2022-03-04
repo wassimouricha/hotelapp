412faf9d216f102527c638a092182853d2077593
wassimouricha <wbouricha5@gmail.com>
2022-03-04 11:10:30 +0100

    latest update

--- lib/bonus.dart
+++ lib/bonus.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hotelapp/main.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 
 const d_blue = Color(0xFF136377);
@@ -11,9 +12,8 @@ const d_lightblue = Color(0xFF25E1ED);
 const d_redus = Color(0xFFFF4A57);
 const d_yellow = Color(0xFFfcec0c);
 
-
 class bonuses extends StatefulWidget {
-  const bonuses({ Key? key }) : super(key: key);
+  const bonuses({Key? key}) : super(key: key);
 
   @override
   State<bonuses> createState() => _bonusesState();
@@ -23,24 +23,31 @@ class _bonusesState extends State<bonuses> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-         child: CircularProgressIndicator(
-            ),
-      )
-        
-
-    );
+        child: Center(
+            child: new CircularPercentIndicator(
+      //ne pas oublier le new sinon le widget ne marche pas
+      radius: 100.0,
+      lineWidth: 10.0,
+      percent: 0.35,
+      center: new Text(
+        "35%",
+        style: GoogleFonts.nunito(
+          color: d_redus,
+          fontSize: 35,
+        ),
+      ),
+      progressColor: d_redus,
+      backgroundColor: d_yellow,
+      circularStrokeCap: CircularStrokeCap.round,
+    )));
   }
 }
-
-
-
 
 class Bonus extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor:  Color(0xff040204),
+        backgroundColor: Color(0xff040204),
         appBar: MyAppBar(),
         body: SingleChildScrollView(
           child: //SingleChildScrollView permet d'empecher l'erreur d'overflow et donc de scroller
@@ -51,7 +58,6 @@ class Bonus extends StatelessWidget {
               //car on va avoir plusieurs widget à l'intérieur
               Card(),
               bonuses(),
-            
             ],
           ),
         ));
@@ -63,8 +69,6 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   String titreDeApp = "NC Hotel Booking";
   String explore = "Bonus Stage";
-
-  
 
   @override
   Widget build(BuildContext context) {
@@ -109,116 +113,100 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   }
 }
 
-
 class descriptionD extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(vertical: 5,horizontal: 5),
-      child:
-      
-      Column(
-        crossAxisAlignment:
-                        CrossAxisAlignment.center,
-        children: 
-      [Container(child: Row(
-       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [Text(
-        
-        "",
-        style: GoogleFonts.nunito(
-          color: d_redus,
-          fontSize: 18,
-          fontWeight: FontWeight.w500,
+      padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+        Container(
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Text(
+              "",
+              style: GoogleFonts.nunito(
+                color: d_redus,
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.star_rate,
+                  color: d_redus,
+                  size: 14.0,
+                ),
+                Icon(
+                  Icons.star_rate,
+                  color: d_redus,
+                  size: 14.0,
+                ),
+                Icon(
+                  Icons.star_rate,
+                  color: d_redus,
+                  size: 14.0,
+                ),
+                Icon(
+                  Icons.star_border,
+                  color: d_redus,
+                  size: 14.0,
+                ),
+                Icon(
+                  Icons.star_border,
+                  color: d_redus,
+                  size: 14.0,
+                ),
+              ],
+            ),
+          ]),
         ),
-      ),
-        Row(
-                    children: [
-                      Icon(
-                        Icons.star_rate,
-                        color: d_redus,
-                        size: 14.0,
-                      ),
-                      Icon(
-                        Icons.star_rate,
-                        color: d_redus,
-                        size: 14.0,
-                      ),
-                      Icon(
-                        Icons.star_rate,
-                        color: d_redus,
-                        size: 14.0,
-                      ),
-                      Icon(
-                        Icons.star_border,
-                        color: d_redus,
-                        size: 14.0,
-                      ),
-                      Icon(
-                        Icons.star_border,
-                        color: d_redus,
-                        size: 14.0,
-                      ),
-                    ],
-                  ),
-      ]),),
-      SizedBox(height:5),
-       ClipRRect(
-  borderRadius: BorderRadius.circular(20),
-  
-  child: 
-  
-  // Image.network("https://i.giphy.com/media/pylNZOmEWUMwGXdzQj/200w.webp"),
-    new Image(image: new AssetImage("assets/cyber-punk2077samurai-dragon.gif"), height:300, width:300,),
-),
-     
-       SizedBox(height:5),
-      
-          Text(
-        "Wake up Samurai... ",
-        textAlign: TextAlign.center,
-        style: GoogleFonts.nunito(
-          color: Colors.white,
-          fontSize: 15,
-          fontWeight: FontWeight.w500,
-          
+        SizedBox(height: 5),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child:
+
+              // Image.network("https://i.giphy.com/media/pylNZOmEWUMwGXdzQj/200w.webp"),
+              new Image(
+            image: new AssetImage("assets/cyber-punk2077samurai-dragon.gif"),
+            height: 300,
+            width: 300,
+          ),
         ),
-      ),
-     
-      
+        SizedBox(height: 5),
+        Text(
+          "Wake up Samurai... ",
+          textAlign: TextAlign.center,
+          style: GoogleFonts.nunito(
+            color: Colors.white,
+            fontSize: 15,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
       ]),
-     
     );
   }
 }
-
-
 
 class Card extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
-        margin: EdgeInsets.all(10),
-        
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: Color(0xff040204),
-          borderRadius: BorderRadius.all(Radius.circular(18)),
-          boxShadow: [
-            BoxShadow(
-              color:  Color(0xff040204),
-              spreadRadius: 4,
-              blurRadius: 6,
-              offset: Offset(0, 3),
-            )
-          ],
-        )
-        ,child: descriptionD(),);
-        
-            
-         
-                
-        
+      margin: EdgeInsets.all(10),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Color(0xff040204),
+        borderRadius: BorderRadius.all(Radius.circular(18)),
+        boxShadow: [
+          BoxShadow(
+            color: Color(0xff040204),
+            spreadRadius: 4,
+            blurRadius: 6,
+            offset: Offset(0, 3),
+          )
+        ],
+      ),
+      child: descriptionD(),
+    );
   }
 }
